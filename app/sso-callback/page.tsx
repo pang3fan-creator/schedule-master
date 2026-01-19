7a2a7ae255576a1d15fa30ee653cb8b2837b064e
pang3fan-creator <pang3fan@gmail.com>
2026-01-19 13:56:37 +0800

    feat: Add SSO callback page for Clerk authentication.

--- app/sso-callback/page.tsx
+++ app/sso-callback/page.tsx
@@ -1,5 +1,13 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { ThemeClerkProvider } from "@/components/ThemeClerkProvider"
 
 export default function SSOCallback() {
-    return <AuthenticateWithRedirectCallback />
+    return (
+        <ThemeProvider>
+            <ThemeClerkProvider locale="en">
+                <AuthenticateWithRedirectCallback />
+            </ThemeClerkProvider>
+        </ThemeProvider>
+    )
 }
